169539b4761cb191363fc2e3040e2d858f686017
AndreRieger2 <144038284+AndreRieger2@users.noreply.github.com>
2024-05-20 14:17:57 -0400

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -65,4 +65,5 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on
+  console.log(`Server is running on port ${PORT}`);
+});
