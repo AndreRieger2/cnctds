c0b4d219d0c5d4c445dcc91fbacfe64bdce24dae
AndreRieger2 <144038284+AndreRieger2@users.noreply.github.com>
2024-05-20 10:59:38 -0400

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -5,43 +5,40 @@ import path from 'path';
 import fs from 'fs';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// Configuração do multer para armazenamento na memória
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// Configuração do multer para lidar com uploads de arquivos
+const upload = multer();
 
-const CLIENT_ID = 'your-client-id';
-const CLIENT_SECRET = 'your-client-secret';
-const REDIRECT_URI = 'your-redirect-uri';
-const REFRESH_TOKEN = 'your-refresh-token';
-const FOLDER_ID = 'your-folder-id';
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const FOLDER_ID = process.env.FOLDER_ID;
 
-// Configuração do Google Drive
-const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+
 oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
 const drive = google.drive({ version: 'v3', auth: oauth2Client });
 
-app.use(express.static(path.join(__dirname, 'views')));
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/upload', upload.single('file'), async (req, res) => {
   try {
-    // Verificar se o arquivo foi enviado corretamente
-    if (!req.file) {
-      return res.status(400).send('No file uploaded');
-    }
-
     const fileMetadata = {
       name: req.file.originalname,
       parents: [FOLDER_ID],
     };
     const media = {
       mimeType: req.file.mimetype,
-      body: Buffer.from(req.file.buffer), // Use o buffer do arquivo diretamente
+      body: req.file.buffer,
     };
     const file = await drive.files.create({
       resource: fileMetadata,
@@ -55,6 +52,6 @@ app.post('/upload', upload.single('file'), async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
