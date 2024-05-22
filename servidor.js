4ba5da695fb1c6ce473acb28e12f68d34b9b76ed
AndreRieger2 <144038284+AndreRieger2@users.noreply.github.com>
2024-05-22 16:01:07 -0400

    Add files via upload

---
+++ servidor.js
@@ -0,0 +1,95 @@
+import express from 'express';
+import multer from 'multer';
+import { google } from 'googleapis';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import { Readable } from 'stream';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const SHEET_ID = process.env.SHEET_ID;  // ID da planilha do Google Sheets
+const FOLDER_ID = process.env.FOLDER_ID;
+
+const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+const drive = google.drive({ version: 'v3', auth: oauth2Client });
+const sheets = google.sheets({ version: 'v4', auth: oauth2Client });
+
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'imagem.html'));
+});
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    if (!req.file || !req.body.username) {
+      return res.status(400).send('No file uploaded or username missing');
+    }
+
+    const bufferStream = new Readable();
+    bufferStream.push(req.file.buffer);
+    bufferStream.push(null);
+
+    const fileMetadata = {
+      name: `${req.body.username} - ${req.body['data[Nome]']}`,
+      parents: [FOLDER_ID],
+    };
+    const media = {
+      mimeType: req.file.mimetype,
+      body: bufferStream,
+    };
+
+    const file = await drive.files.create({
+      resource: fileMetadata,
+      media: media,
+      fields: 'id',
+    });
+
+    const formData = req.body;
+    const sheetData = [
+      formData['data[Nome]'],
+      formData['data[Email]'],
+      formData['data[Número]'],
+      formData['data[Data]'],
+      formData['data[Sexo]'],
+      formData['data[Líder]'],
+      formData['data[Cidade]'],
+      req.body.username,
+      file.data.id  // ID do arquivo no Google Drive
+    ];
+
+    await sheets.spreadsheets.values.append({
+      spreadsheetId: SHEET_ID,
+      range: 'Sheet1!A1', // Ajuste para o intervalo adequado
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values: [sheetData],
+      },
+    });
+
+    res.redirect('/success.html');
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    res.status(500).send('Error uploading file');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
