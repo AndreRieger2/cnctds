f7def0d3bb43213a1bf85ded96d543ee402d5eac
AndreRieger2 <144038284+AndreRieger2@users.noreply.github.com>
2024-05-23 15:31:20 -0400

    telefone

--- servidor.js
+++ servidor.js
@@ -77,7 +77,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
         const sheetData = [
             formData.Nome,
             formData.Email,
-            formData.Numero,  // Corrigido para Numero
+            formData.Telefone,  // Corrigido para Numero
             formData.Data,
             formData.Sexo,
             formData.Lider,
